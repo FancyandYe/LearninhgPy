31d4383a3a208d44bfe358572a4507aff38978ee
樊晨雨 <fanchenyu@gracepsy.com>
2019-05-07 20:32:17 +0800

    first commit

---
+++ day01-05/code/first.py
@@ -0,0 +1,14 @@
+print(100 + 100)
+print('This is a', 'small', 'dog')
+print('1024 * 768 = ', 1024 * 768)
+a = 100
+if a >= 100:
+    print(a)
+else:
+    print(-a)
+b = True
+c = True
+d = False
+print( b and c, b or c, not c)
+s3 = 'Hello, "Bart"'
+print(s3)
